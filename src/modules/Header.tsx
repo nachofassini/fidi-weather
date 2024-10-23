61b17cfa68a22fc9bedc127ad637748d2e8b5e12
Ignacio Fassini <nachofassini@gmail.com>
2024-10-23 13:59:53 -0300

    Add links on header

--- src/modules/Header.tsx
+++ src/modules/Header.tsx
@@ -1,14 +1,19 @@
 import Image from "next/image";
 import logo from "@/app/assets/logo.svg";
 import { DarkThemeToggle } from "flowbite-react";
+import Link from "next/link";
 
 export const Header = () => (
   <header className="w-full h-16 md:h-20 p-4 flex justify-between items-center">
-    <Image src={logo} alt="Fidi Weather" className="dark:invert" priority />
+    <Link href="/">
+      <Image src={logo} alt="Fidi Weather" className="dark:invert" priority />
+    </Link>
 
     <DarkThemeToggle className="ml-auto mr-2" />
-    <h2 className="text-xl md:text-2xl font-bold text-gray-700 dark:text-white">
-      Weather APP
-    </h2>
+    <Link href="/">
+      <h2 className="text-xl md:text-2xl font-bold text-gray-700 dark:text-white">
+        Weather APP
+      </h2>
+    </Link>
   </header>
 );
