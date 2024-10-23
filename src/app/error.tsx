73e742f48061e6271216a25391b71be747db81b7
Ignacio Fassini <nachofassini@gmail.com>
2024-10-23 13:59:40 -0300

    Fix error page UI

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { Button } from "flowbite-react";
 import Link from "next/link";
 
 export default function Error() {
   return (
     <div>
-      <p className="text-5xl">Upps, ocurrio un error. ¡Intentá de nuevo!</p>;
-      <Link href="/">Volver al inicio</Link>
+      <p className="text-5xl">Upps, ocurrio un error. ¡Intentá de nuevo!</p>
+      <Link href="/">
+        <Button className="mt-2">Volver al inicio</Button>
+      </Link>
     </div>
   );
 }
